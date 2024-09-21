85cd7c96d2ddd2c43b945ab28ce3e2082d8a6ffc
肖祺彦 <572752189@qq.com>
2024-09-21 21:03:14 +0800

    前后端分离管理

--- backend/app/controller/user.js
+++
@@ -1,9 +0,0 @@
-const { Controller } = require('egg');
-class UserController extends Controller {
-  async login() {
-    const { ctx } = this;
-    ctx.body = await this.service.user.login();
-  }
-}
-
-module.exports = UserController;
--- backend/app/router/user.js
+++
@@ -1,5 +0,0 @@
-module.exports = app => {
-  const { router, controller } = app;
-
-  router.post('/api/v1/user/login', controller.user.login); // 用户登陆
-};
